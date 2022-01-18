437b70f31c0738ac3db57d55eebc9e416ab4759c
samar768 <samkumar768@gmail.com>
2022-01-18 21:25:08 +0200

    Add project files.

---
+++ Controllers/StudentDetailsController.cs
@@ -0,0 +1,59 @@
+﻿using CRUD_BAL.Service;
+using CRUD_DAL.Interface;
+using CRUD_DAL.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDAspNetCore5WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentDetailsController : ControllerBase
+    {
+        private readonly StudentService _studentService;
+
+        private readonly IRepositoryStudent<StudentDetails> _studentrepository;
+
+        public StudentDetailsController(IRepositoryStudent<StudentDetails> studentRepository, StudentService studentservice)
+        {
+            _studentService = studentservice;
+            _studentrepository = studentRepository;
+
+        }
+        //Add Person
+        [HttpPost("Addstudent")]
+        public async Task<Object> AddStudent([FromBody] StudentDetails student)
+        {
+            try
+            {
+                await _studentService.AddStudent(student);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+        //GET All Person
+        [HttpGet("GetAllStudent")]
+        public Object GetAllStudents()
+        {
+            var data = _studentService.GetAllStudent();
+            var json = JsonConvert.SerializeObject(data, Formatting.Indented,
+                new JsonSerializerSettings()
+                {
+                    ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+                }
+            );
+            return json;
+        }
+
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,79 @@
+using CRUD_BAL.Service;
+using CRUD_DAL.Data;
+using CRUD_DAL.Interface;
+using CRUD_DAL.Models;
+using CRUD_DAL.Repository;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDAspNetCore5WebAPI
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        //Named constructor
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+   
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllers();
+            services.AddHttpClient();
+
+
+                    
+            services.AddTransient<IRepository<Person>, RepositoryPerson>();
+            services.AddTransient<IRepositoryStudent<StudentDetails>, RepositoryStudent>();
+
+            services.AddTransient<PersonService, PersonService>();
+            services.AddTransient<StudentService, StudentService>();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CRUDAspNetCore5WebAPI", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CRUDAspNetCore5WebAPI v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
